53634f7f4c0390ce9914573f7e89b24a840b0957
Tanmay Patil <tanmayprafullapatil@gmail.com>
2026-02-16 14:11:20 +0530

    initial commit

---
+++ models/listing.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    image: {
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1770347265612-91f67721344e?q=80&w=1175&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        },
+        filename: {
+            type: String,
+            default: "default_image"
+        }
+    },
+    price: Number,
+    location: String,
+    country: String
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
